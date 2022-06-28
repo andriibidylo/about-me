5c2ccf01c11a919b6f086dffa24d1f0026ebee6f
andrewbidylo <77558037+andrewbidylo@users.noreply.github.com>
2022-06-28 15:30:26 -0700

    add Header styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 
-import Container from "@mui/material/Container";
-import { Home } from "./pages/Home";
+import Container from "@mui/material/Container"
+import { Home } from "./pages/Home"
+import {Header} from "./components/Header"
+
 
 const App = () => {
   return (
     <>
+    <Header/>
       <Container maxWidth="lg">
       <Home />
       </Container>
