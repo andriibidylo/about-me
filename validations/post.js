61652e4e143d69979e8db947c9f3487a8e095b6d
andrewbidylo <77558037+andrewbidylo@users.noreply.github.com>
2022-06-27 22:03:54 -0700

    add PostSchema

--- validations/post.js
+++ validations/post.js
@@ -5,5 +5,5 @@ export const CreatePostValidation = [
   body("title", "Please add a title").isLength({min:3}).isString(),
   body("text", "Please add text").isLength({min:10}).isString(),
   body("tags", "Tags format not correct").optional().isArray(),
-  body("imageUrl", "Link not correct").optional().isURL(),
+  body("imageUrl", "Link not correct").optional().isString(),
 ]
