1c668d47ee75921cae4ddd809dbc032f3e93a75f
andrewbidylo <77558037+andrewbidylo@users.noreply.github.com>
2022-06-28 13:32:28 -0700

    add client/server folders

--- validations/post.js
+++
@@ -1,9 +0,0 @@
-import {body} from "express-validator"
-
-
-export const createPostValidation = [
-  body("title", "Please add a title").isLength({min:3}).isString(),
-  body("text", "Please add text").isLength({min:10}).isString(),
-  body("tags", "Tags format not correct").optional().isString(),
-  body("imageUrl", "Link not correct").optional().isString(),
-]
