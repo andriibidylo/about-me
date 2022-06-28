33e0ec8a607002dd8f7f6baa0e4e28f017b1dcc1
andrewbidylo <77558037+andrewbidylo@users.noreply.github.com>
2022-06-28 13:31:02 -0700

    add client/server folders

---
+++ server/controllers/PostController.js
@@ -0,0 +1,132 @@
+import PostModel from "../models/Post.js"
+
+export const getAllPosts = async (req, res) => {
+  try {
+    const post = await PostModel.find().populate("author").exec()
+    res.json(post)
+
+  } catch (error) {
+    console.log(error)
+    res.status(500)
+    res.json({ message: "Could not show all posts" })
+  }
+}
+
+export const getOnePost = async (req, res) => {
+  try {
+    const postId = req.params.id;
+
+    PostModel.findOneAndUpdate(
+      {
+        _id: postId,
+      },
+      {
+        $inc: { viewsCount: 1 },
+      },
+      {
+        returnDocument: 'after',
+      },
+      (err, doc) => {
+        if (err) {
+          console.log(err);
+          return res.status(500).json({
+            message: 'Return the post - error',
+          });
+        }
+
+        if (!doc) {
+          return res.status(404).json({
+            message: 'Post not found',
+          });
+        }
+
+        res.json(doc);
+      },
+    ).populate('user');
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: 'Return the post - error',
+    });
+  }
+}
+export const createPost = async (req, res) => {
+  try {
+    const doc = new PostModel ({
+      title: req.body.title,
+      text: req.body.text,
+      tags: req.body.tags,
+      imageUrl: req.body.imageUrl,
+      author: req.userId
+    })
+    const post = await doc.save()
+    res.json(post)
+
+  } catch (error) {
+    console.log(error)
+    res.status(500)
+    res.json({ message: "Post did not create" })
+  }
+}
+export const removePost = async (req, res) => {
+
+    try {
+      const postId = req.params.id;
+  
+      PostModel.findOneAndDelete(
+        {
+          _id: postId,
+        },
+        (err, doc) => {
+          if (err) {
+            console.log(err);
+            return res.status(500).json({
+              message: 'Post did not delete',
+            });
+          }
+  
+          if (!doc) {
+            return res.status(404).json({
+              message: 'Post not found',
+            });
+          }
+  
+          res.json({
+            success: true,
+          });
+        },
+      );
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({
+        message: 'Post did not get',
+      });
+    }
+}
+export const updatePost = async (req, res) => {
+  try {
+    const postId = req.params.id;
+
+    await PostModel.updateOne(
+      {
+        _id: postId,
+      },
+      {
+        title: req.body.title,
+        text: req.body.text,
+        imageUrl: req.body.imageUrl,
+        author: req.userId,
+        tags: req.body.tags.split(','),
+      },
+    );
+
+    res.json({
+      success: true,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: 'Post did not update',
+    });
+  }
+}
---
+++ server/validations/auth.js
@@ -0,0 +1,14 @@
+import {body} from "express-validator"
+
+
+export const registerValidation = [
+  body("email", "Wrong email format").isEmail(),
+  body("password", "Password must be at least 5 characters").isLength({min: 5}),
+  body("fullName", "Full name must be at least 3 characters").isLength({min: 3}),
+  body("avatartUrl", "Wrong avatar ull").optional().isURL(),
+]
+
+export const loginValidation = [
+  body("email", "Wrong email format").isEmail(),
+  body("password", "Password must be at least 5 characters").isLength({min: 5}),
+]
