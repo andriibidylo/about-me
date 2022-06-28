e6fd3f9b0d096cc5b21c693c20924c182d1d336b
andrewbidylo <77558037+andrewbidylo@users.noreply.github.com>
2022-06-27 21:11:59 -0700

    add get aouth/me route

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ import { validationResult } from "express-validator"
 import { registerValidation } from "./validations/auth.js"
 import UserModel from "./models/User.js"
 import bcrypt from 'bcrypt'
+import checkAuth from "./utils/checkAuth.js"
+import User from "./models/User.js"
 
 const app = express()
 
@@ -21,57 +23,57 @@ app.post("/auth/register", registerValidation, async (req, res) => {
   const errors = validationResult(req)
   if (!errors.isEmpty()) {
     return res.status(400).json(errors.array())
-  } 
+  }
   try {
-  const password = req.body.password;
-  const salt = await bcrypt.genSalt(10);
-  const hash = await bcrypt.hash(password, salt);
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
 
 
 
-  const doc = new UserModel({
-    email: req.body.email,
-    fullName: req.body.fullName,
-    avatarUrl: req.body.avatarUrl,
-    passwordHash: hash,
-  });
-  const user = await doc.save();
-  
-  const token = jwt.sign({
-    _id: user._id
-  }, "secretkey",{
-    expiresIn: "20min"
-  })  
-const {passwordHash, ...userData} = user._doc
-  res.json({
-    ...userData,
-    token
-  })
+    const doc = new UserModel({
+      email: req.body.email,
+      fullName: req.body.fullName,
+      avatarUrl: req.body.avatarUrl,
+      passwordHash: hash,
+    });
+    const user = await doc.save();
 
-}catch(err){
-  console.log(err)
-      res.status(500)
-      res.json({message: "We could not register you"})
-}
+    const token = jwt.sign({
+      _id: user._id
+    }, "secretkey", {
+      expiresIn: "20min"
+    })
+    const { passwordHash, ...userData } = user._doc
+    res.json({
+      ...userData,
+      token
+    })
+
+  } catch (err) {
+    console.log(err)
+    res.status(500)
+    res.json({ message: "We could not register you" })
+  }
 })
 
-app.post ("/auth/login", async(req, res)=> {
+app.post("/auth/login", async (req, res) => {
   try {
-    const user = await UserModel.findOne({email: req.body.email})
-    if (!user){
-      return res.status(404).json({message: "User not found"})
+    const user = await UserModel.findOne({ email: req.body.email })
+    if (!user) {
+      return res.status(404).json({ message: "User not found" })
     }
     const isValidPassword = await bcrypt.compare(req.body.password, user._doc.passwordHash)
 
     if (!isValidPassword) {
-      return res.status(400).json({message: "Wrong email or password"})
+      return res.status(400).json({ message: "Wrong email or password" })
     }
     const token = jwt.sign({
       _id: user._id
-    }, "secretkey",{
+    }, "secretkey", {
       expiresIn: "20min"
-    })  
-  const {passwordHash, ...userData} = user._doc
+    })
+    const { passwordHash, ...userData } = user._doc
     res.json({
       ...userData,
       token
@@ -80,7 +82,24 @@ app.post ("/auth/login", async(req, res)=> {
   } catch (error) {
     console.log(err)
     res.status(500)
-    res.json({message: "Authorization error"})
+    res.json({ message: "Authorization error" })
+  }
+})
+
+app.get("/auth/me", async (req, res) => {
+  try {
+    const user = await UserModel.findById(req.userId)
+    if (!user) {
+      return res.status(404).json({ message: "User not found" })
+    }
+    const { passwordHash, ...userData } = user._doc
+    res.json({
+      ...userData
+    })
+  } catch (error) {
+    console.log(err)
+    res.status(404)
+    res.json({ message: "Access denied" })
   }
 })
 app.listen(8000, (err) => {
