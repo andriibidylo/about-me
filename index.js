b366b27848e1123aa9eb01d3282c3dff4c8b495e
andrewbidylo <77558037+andrewbidylo@users.noreply.github.com>
2022-06-27 17:11:54 -0700

    add User model

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 import express, { json } from "express"
+import jwt from "jsonwebtoken"
+import mongoose from "mongoose"
+
+
+
 
 const app = express()
+
+mongoose.connect("mongodb+srv://Andrew:CaFMA3g6N8mFmgPo@cluster0.kjby4.mongodb.net/?retryWrites=true&w=majority")
+.then(console.log("DB connected"))
+.catch((err)=> console.log("DB error"+err))
 
 app.use(express.json())
 
@@ -8,10 +17,10 @@ app.get("/",(req, res)=> {
   res.send("Hello")
 })
 app.post("/auth/login",(req, res)=> {
-  console.log(req.body)
-  res.json({
-    "success": true
-  })
+ const token = jwt.sign({
+   email: req.body.email,
+   fullName: req.body.name
+ }, "secretkey") 
 })
 
 app.listen(8000, (err)=>{
