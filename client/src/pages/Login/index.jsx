5b0630a84d29af41bda8c5174f11bb2c1ae765c7
andrewbidylo <77558037+andrewbidylo@users.noreply.github.com>
2022-06-30 04:23:00 -0700

    add react-hook-form to login page

--- client/src/pages/Login/index.jsx
+++ client/src/pages/Login/index.jsx
@@ -1,28 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
 import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
-
+import { useDispatch, useSelector } from 'react-redux'
 import styles from "./Login.module.scss";
+import { fetchUserData } from "../../redux/auth/slice"
+import { useForm } from "react-hook-form"
 
 export const Login = () => {
+
+  const { register, handleSubmit, setError, formState: { errors, isValid } } = useForm({
+    defaultValues: {
+      email: "",
+      password: ""
+    }
+  })
+
+  const onSubmit = (value) => {
+    dispatch(fetchUserData(value))
+  }
+
+  const dispatch = useDispatch()
+
   return (
     <Paper classes={{ root: styles.root }}>
       <Typography classes={{ root: styles.title }} variant="h5">
         Log in
       </Typography>
+      <form onSubmit={handleSubmit(onSubmit)}>
       <TextField
         className={styles.field}
         label="E-Mail"
-        error
-        helperText="Wrong email"
+        error={Boolean(errors.email?.message)}
+        helperText={errors.email?.message}
         fullWidth
+        type="email"
+        {...register("email", {required: "Please enter an email"})}
+
       />
-      <TextField className={styles.field} label="Password" fullWidth />
-      <Button size="large" variant="contained" fullWidth>
+      <TextField
+        className={styles.field}
+        error={Boolean(errors.password?.message)}
+        helperText={errors.password?.message}
+        label="Password"
+        {...register("password", {required: "Please enter a password"})}
+        fullWidth />
+      <Button size="large" variant="contained" type="submit" fullWidth>
         Log out
       </Button>
+      </form>
     </Paper>
   );
 };
