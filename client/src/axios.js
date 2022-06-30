7a655bbfe174b089299bc26ef7a224eb16e0f333
andrewbidylo <77558037+andrewbidylo@users.noreply.github.com>
2022-06-30 14:21:18 -0700

    configure axios and add token to header requests

--- client/src/axios.js
+++ client/src/axios.js
@@ -3,5 +3,9 @@ import axios from "axios"
 const instance = axios.create({
   baseURL: "http://localhost:8000",
 })
+instance.interceptors.request.use((config)=>{
+  config.headers.Authorization = window.localStorage.getItem("token")
+  return config
+})
 
 export default instance
