0e205b18a92d219b160010b0a777b83607020696
andrewbidylo <77558037+andrewbidylo@users.noreply.github.com>
2022-07-21 10:53:41 -0700

    refactored/ add Search component

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,6 +1,5 @@
-import React, { useState, useCallback, useRef } from 'react';
-import { Button, IconButton, TextField, Container } from '@mui/material';
-import ClearIcon from '@mui/icons-material/Clear';
+import React, { useState } from 'react';
+import { Button, Container } from '@mui/material';
 import { Link } from "react-router-dom"
 import styles from './Header.module.scss';
 import { selectAuth } from "../../redux/auth/selectors"
@@ -8,35 +7,17 @@ import { useDispatch, useSelector } from 'react-redux'
 import { logout } from "../../redux/auth/slice";
 import { UserInfo } from '../UserInfo';
 import { useNavigate } from "react-router"
-import debounce from 'lodash.debounce'
-import { setSearchValue, setDefaultSearchValues } from '../../redux/filters/slice'
-
+import { setDefaultSearchValues } from '../../redux/filters/slice'
+import { Search } from '../Search/index'
 
 
 export const Header = () => {
 
-  const searchRef = useRef(null)
   const [searchText, setSearchText] = useState("")
   const naviagate = useNavigate()
   const dispatch = useDispatch()
   const { authorizedUser } = useSelector(selectAuth)
   const isAuth = Boolean(authorizedUser)
-
-
-  const memorizedSearchValue = useCallback(debounce(str => {
-    dispatch(setSearchValue(str))
-  }, 350), [])
-
-  const onChangeInput = (text) => {
-    setSearchText(text)
-    memorizedSearchValue(text)
-  }
-
-  const clearSearchValue = () => {
-    dispatch(setSearchValue(""))
-    setSearchText("")
-    searchRef.current?.focus()
-  }
 
   const onClickLogout = () => {
     dispatch(logout())
@@ -56,31 +37,7 @@ export const Header = () => {
           <div onClick={onClickLogo} className={styles.logo} >
             <div>ABOUT ME APP</div>
           </div>
-          <div className={styles.searchField} >
-
-            <TextField onChange={(e) => onChangeInput(e.target.value)}
-              value={searchText}
-              className={styles.search}
-              size="small"
-              id="outlined-basic"
-              label="Search..."
-              variant="outlined"
-              inputRef={searchRef}
-              InputProps={{
-                endAdornment: (
-                  <IconButton
-                    sx={{ visibility: searchText ? "visible" : "hidden" }}
-                    onClick={clearSearchValue}
-                  >
-                    <ClearIcon />
-                  </IconButton>
-                ),
-              }}
-              sx={{
-                m: 2,
-                "& .Mui-focused .MuiIconButton-root": { color: "primary.main" },
-              }} />
-          </div>
+          <Search setSearchText={setSearchText} searchText={searchText} se />
           <div className={styles.inner}>
             {isAuth && <UserInfo {...authorizedUser} />}
             <div className={styles.buttons}>
